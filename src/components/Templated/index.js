f7902530ffd3a904759f4f05c666663642cd2668
Andres Kovalev <mail@akovalev.ru>
2019-11-13 19:29:32 +0100

    docs: added documentation for basic features

--- src/components/Templated/index.js
+++ src/components/Templated/index.js
@@ -6,6 +6,41 @@ const Content = require('../Content');
 
 module.exports = Templated;
 
+/**
+ * @param {React.node} content content to inject into template
+ * @param {React.children} children template component (with slots)
+ * @description Injects content into template component.
+ * @docs
+ * When you have a template component created using [slots](../Slot/README.md)
+ * and content to inject (component created using [Content](../Content/README.md)),
+ * you can perform injection using `Templated` component:
+ *
+ * ```js
+ * // Page.js
+ * import { Templated } from 'react-templated';
+ * import PageTemplate from './PageTemplate.js';
+ *
+ * export default ({ children }) => (
+ *     <Templated content={ children }>
+ *         <PageTemplate />
+ *     </Templated>
+ * );
+ * ```
+ *
+ * If our template component consumes some props, we can pass those through:
+ *
+ * ```js
+ * ...
+ *
+ * export default ({ children, ...props }) => (
+ *     <Templated content={ children }>
+ *         <PageTemplate { ...props } />
+ *     </Templated>
+ * );
+ * ```
+ *
+ * We can achieve similar behaviour by using [withSlots() hook](../withSlots/README.md).
+ */
 function Templated({ content, children }) {
     const contentMap = {};
 
--- tools/prepare-readme.sh
+++ tools/prepare-readme.sh
@@ -3,3 +3,6 @@
 set -e
 
 node ./tools/generate-readmes.js
+node ./tools/jsdoc-to-markdown.js ./src/components/README.hbs --components "./src/components/*/index.js" > ./src/components/README.md
+node ./tools/jsdoc-to-markdown.js ./README.hbs --components "./src/components/*/index.js" > ./README.md
+node ./tools/jsdoc-to-markdown.js ./SUMMARY.hbs --components "./src/components/*/index.js" > ./SUMMARY.md
